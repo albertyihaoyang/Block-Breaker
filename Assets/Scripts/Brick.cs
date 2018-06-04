6e43f8adcea5b536fb3f6e1d0b8f1e6707e8ceb8
Yihao Yang <yihaoyan@usc.edu>
2018-06-03 19:00:28 -0700

    First Commit

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+    public Sprite[] hitSprites;
+    public static int brickableCount = 0;
+    private bool isBreakable;
+    public GameObject smoke;
+
+    public AudioClip crack;
+    private int timesHit;
+    private LevelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+        isBreakable = (this.tag == "Breakable");
+        if (isBreakable){
+            brickableCount++;
+        }
+        timesHit = 0;
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col){
+        if (isBreakable){
+            HandleHits();
+        }
+    }
+
+    void HandleHits(){
+        timesHit++;
+        if (timesHit > hitSprites.Length)
+        {
+            brickableCount--;
+            if (brickableCount <= 0){
+                levelManager.LoadNextLevel();
+            }
+            GameObject smokePuff = Instantiate(smoke, gameObject.transform.position, Quaternion.identity) as GameObject;
+            smokePuff.GetComponent<ParticleSystem>().startColor = gameObject.GetComponent<SpriteRenderer>().color;
+            Destroy(gameObject);
+        }
+        else
+        {
+            LoadSprites();
+        }
+    }
+
+    void LoadSprites(){
+        int spritIndex = timesHit - 1;
+        if (hitSprites[spritIndex])
+        {
+            this.GetComponent<SpriteRenderer>().sprite = hitSprites[spritIndex];
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    public bool autoPlay = false;
+    private Ball ball;
+
+	// Use this for initialization
+	void Start () {
+        ball = GameObject.FindObjectOfType<Ball>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!autoPlay)
+        {
+            MoveWithMouse();
+        } else {
+            AutoPlay();
+        }
+    }
+
+    void AutoPlay(){
+        Vector3 ballPos = ball.transform.position;
+        this.transform.position = new Vector3(Mathf.Clamp(ballPos.x, 0.5f, 15.5f), 0.5f, 0f);
+    }
+
+    void MoveWithMouse(){
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+        this.transform.position = new Vector3(Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f), 0.5f, 0f);
+    }
+}
